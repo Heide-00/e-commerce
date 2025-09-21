2639334bd0f97a9f9ddeadf19a2a6d5819cc056a
Heide-00 <gulaslan93@gmail.com>
2025-09-21 18:36:39 +0300

    Create HomePage.jsx
    HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import TopBar from "../layout/TopBar";
+import Header from "../layout/Header";
+import HeroSlider from "../components/HeroSlider";
+import PageContent from "../layout/PageContent";
+import ProductSection from "../components/ProductSection";
+import CategorySection from "../components/CategorySection";
+import SliderSection from "../components/SliderSection";
+import PromoBanner from "../components/PromoBanner";
+import BlogSection from "../components/BlogSection";
+import FooterContent from "../components/FooterContent";
+
+export default function HomePage() {
+  return (
+    <div className="w-full">
+     <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20">
+        <HeroSlider />
+     <main className="flex flex-col gap-12 py-8">
+       <CategorySection />
+       <ProductSection />
+       <SliderSection/>
+       <PromoBanner/>
+       <BlogSection/>
+       <FooterContent/>
+      </main>
+    </div>
+     </div>
+  );
+}
