00ab61be38ac25b8a90e2ef9abb776e60fb0a740
Heide-00 <gulaslan93@gmail.com>
2025-09-21 18:34:58 +0300

    Create Footer.jsx
    Footer.jsx

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+import FooterContent from "../components/FooterContent";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 px-6 py-10">
+      <FooterContent />
+    </footer>
+  );
+}
