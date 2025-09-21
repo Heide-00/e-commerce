ad6c6dd0f9a25bfb7e82055d91a3c25d7f2005f1
Heide-00 <gulaslan93@gmail.com>
2025-09-21 18:02:25 +0300

    Create CategoryCard.jsx
    CategoryCard.jsx

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,15 @@
+export default function CategoryCard({ image, label }) {
+  return (
+    <div className="relative w-full aspect-[3/4] max-w-[300px] overflow-hidden rounded-md bg-white">
+      <img
+        src={image}
+        alt={label}
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute bottom-4 left-4 bg-white px-3 py-1 text-sm font-semibold text-black-900">
+        {label}
+      </div>
+    </div>
+  )
+}
+
