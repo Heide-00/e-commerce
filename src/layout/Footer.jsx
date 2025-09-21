cec44b68553283e3bd6be60fc3e2682742330307
Heide-00 <gulaslan93@gmail.com>
2025-09-22 01:10:10 +0300

    Update Footer.jsx
    Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,8 +2,10 @@ import FooterContent from "../components/FooterContent";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 px-6 py-10">
-      <FooterContent />
+    <footer className="bg-gray-100">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 py-10">
+        <FooterContent />
+      </div>
     </footer>
   );
 }
