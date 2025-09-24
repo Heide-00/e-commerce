3bb310eb41816b11e910df9ac6d4b08848b5f052
Heide-00 <gulaslan93@gmail.com>
2025-09-24 22:28:13 +0300

    Update ProductDetail.jsx
    ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
-import { products } from "../components/ProductSection";
+//import { products } from "../components/ProductSection";
+import { products } from "../mock/products.js";
 
 export default function ProductDetail() {
   const { id } = useParams();
