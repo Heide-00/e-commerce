e4caafb22622f5b527de67436a3af7a2bb8e853a
Heide-00 <gulaslan93@gmail.com>
2025-09-24 22:27:07 +0300

    Create products.js
    product.js

---
+++ src/mock/products.js
@@ -0,0 +1,83 @@
+import fixed0 from "../assets/images/product-cards/fixed-height.png";
+import fixed1 from "../assets/images/product-cards/fixed-height1.png";
+import fixed2 from "../assets/images/product-cards/fixed-height2.png";
+import fixed3 from "../assets/images/product-cards/fixed-height3.png";
+import fixed4 from "../assets/images/product-cards/fixed-height4.png";
+import fixed5 from "../assets/images/product-cards/fixed-height5.png";
+import fixed6 from "../assets/images/product-cards/fixed-height6.png";
+import fixed7 from "../assets/images/product-cards/fixed-height7.png";
+
+export const products = [
+  {
+    id: "0",
+    image: fixed0,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "1",
+    image: fixed1,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "2",
+    image: fixed2,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "3",
+    image: fixed3,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "4",
+    image: fixed4,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "5",
+    image: fixed5,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "6",
+    image: fixed6,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+  {
+    id: "7",
+    image: fixed7,
+    title: "Graphic Design",
+    department: "English Department",
+    price: "$6.48",
+    oldPrice: "$16.48",
+    colors: ["blue", "green", "orange", "black"],
+  },
+];
