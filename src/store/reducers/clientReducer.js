3616440c4bff3f2b3da286961518ad1be6969871
Heide-00 <gulaslan93@gmail.com>
2025-09-25 17:49:16 +0300

    Update clientReducer.js
    clientReducer.js

--- src/store/reducers/clientReducer.js
+++ src/store/reducers/clientReducer.js
@@ -1,26 +1,35 @@
 const initialState = {
-  user: {},
-  addressList: [],
-  creditCards: [],
+  user: null,
   roles: [],
   theme: 'light',
   language: 'en',
+  addressList: [],
+  creditCards: []
 };
 
 export default function clientReducer(state = initialState, action) {
   switch (action.type) {
     case 'SET_USER':
       return { ...state, user: action.payload };
+
+    case 'LOGIN_SUCCESS':
+      return { ...state, user: action.payload };
+
     case 'SET_ROLES':
       return { ...state, roles: action.payload };
+
     case 'SET_THEME':
       return { ...state, theme: action.payload };
+
     case 'SET_LANGUAGE':
       return { ...state, language: action.payload };
+
     case 'SET_ADDRESS_LIST':
       return { ...state, addressList: action.payload };
+
     case 'SET_CREDIT_CARDS':
       return { ...state, creditCards: action.payload };
+
     default:
       return state;
   }
