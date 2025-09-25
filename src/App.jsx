f92d5202fbec1aea840152122d267e0cf0d8735e
Heide-00 <gulaslan93@gmail.com>
2025-09-25 22:54:14 +0300

    Update App.jsx
    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -14,8 +16,39 @@ import TeamPage from "./pages/TeamPage";
 import AboutPage from "./pages/AboutPage";
 import SignupPage from "./pages/SignupPage";
 import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+
+import axiosInstance, { setAuthToken, clearAuthToken } from "./api/axiosInstance";
+import { setUser } from "./store/actions/clientActions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    //Token'ı axios header'a ekle
+    setAuthToken(token);
+
+    //Token doğrulama isteği
+    axiosInstance.get("/verify")
+      .then((res) => {
+        //Redux'a kullanıcıyı yaz
+        dispatch(setUser(res.data.user));
+
+        //Token'ı yenile
+        localStorage.setItem("token", "mock-token-123");
+        setAuthToken("mock-token-123");
+      })
+      .catch(() => {
+        //Token geçersizse temizle
+        localStorage.removeItem("token");
+        clearAuthToken();
+        dispatch({ type: "CLEAR_USER" });
+      });
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <div className="w-full">
@@ -32,13 +65,13 @@ function App() {
             <Route path="/about" component={AboutPage} />
             <Route path="/signup" component={SignupPage} />
             <Route path="/cart" component={CartPage} />
+            <Route path="/login" component={LoginPage} />
           </Switch>
 
           <Footer />
         </div>
       </div>
 
-      {/* Toaster mesajları için */}
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -52,7 +85,6 @@ function App() {
 }
 
 export default App;
-
 
 
 
