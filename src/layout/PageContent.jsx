465477385ed7b63995fbd41fe8e677d70e739d46
Heide-00 <gulaslan93@gmail.com>
2025-09-27 13:32:12 +0300

    Update PageContent.jsx
    PageContent

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -2,7 +2,7 @@ export default function PageContent({ title, children }) {
   return (
     <section className="text-center py-8">
       <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
-      <p className="text-sm text-gray-600 mt-2">{children}</p>
+      <div className="text-sm text-gray-600 mt-2">{children}</div>
     </section>
   );
 }
