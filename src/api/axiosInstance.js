fdf9b5c36ce96e1a256d6a1e122eef776a53de40
Heide-00 <gulaslan93@gmail.com>
2025-09-27 13:27:06 +0300

    Update axiosInstance.js
    axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://workintech-fe-ecommerce.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Cache-Control": "no-cache"
+  },
+  responseType: "json",
+  timeout: 10000
 });
 
-// Token'ı header'a eklemek için
+//Token'ı header'a ekleme
 export const setAuthToken = (token) => {
-  axiosInstance.defaults.headers.common["Authorization"] = token;
+  axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
 
-// Token'ı header'dan silmek için
+//Token'ı header'dan silme
 export const clearAuthToken = () => {
   delete axiosInstance.defaults.headers.common["Authorization"];
 };
