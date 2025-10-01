314cb72b585789af0793f174a8b066944e1418a1
Heide-00 <gulaslan93@gmail.com>
2025-10-02 00:07:24 +0300

    Update vite.config.js
    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   test: {
-    environment: 'jsdom', // ← DOM hatasını çözen kritik satır
-    globals: true,         // test ve expect gibi global fonksiyonları tanır
+    environment: 'jsdom', 
+    globals: true,         
   }
  }) 
+
+
+
