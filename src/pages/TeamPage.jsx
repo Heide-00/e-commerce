8aca2a380cb4c0b6296ec3bf83ab21a8d37d8305
Heide-00 <gulaslan93@gmail.com>
2025-10-02 00:03:08 +0300

    Update TeamPage.jsx
    TeamPage.jsx

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -45,14 +45,7 @@ export default function TeamPage() {
 
   return (
     <PageContent title="Ekibimiz">
-      {/*Rol verisini sayfa üstünde bir kez gösterme */}
-      {roles.length > 0 && (
-        <div className="text-sm text-gray-500 mb-6 text-center">
-          Rol verisi yüklendi: {roles.length} adet rol
-        </div>
-      )}
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {team.map((member, index) => (
           <div
             key={index}
