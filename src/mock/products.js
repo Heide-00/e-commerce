87309aa5c4b44361a55137a44724630673f862ea
Heide-00 <gulaslan93@gmail.com>
2025-10-02 00:00:10 +0300

    Update products.js
    products.js

--- src/mock/products.js
+++ src/mock/products.js
@@ -7,9 +7,28 @@ import fixed5 from "../assets/images/product-cards/fixed-height5.png";
 import fixed6 from "../assets/images/product-cards/fixed-height6.png";
 import fixed7 from "../assets/images/product-cards/fixed-height7.png";
 
+
+import womenBag1 from "../assets/images/categories/womenbag1.png"
+import womenBag2 from "../assets/images/categories/womenbag2.png";
+import womenBelt1 from "../assets/images/categories/womenbelt1.png";
+import womenBelt2 from "../assets/images/categories/womenbelt2.png";
+import womenCosmetic1 from "../assets/images/categories/womencosmetic1.png";
+import womenCosmetic2 from "../assets/images/categories/womencosmetic2.png";
+import womenHat1 from "../assets/images/categories/womenhat1.png";
+import womenHat2 from "../assets/images/categories/womenhat2.png";
+import menBag1 from "../assets/images/categories/menbag1.png";
+import menBag2 from "../assets/images/categories/menbag2.png";
+import menBelt1 from "../assets/images/categories/menbelt1.png";
+import menBelt2 from "../assets/images/categories/menbelt2.png";
+import menHat1 from "../assets/images/categories/menhat1.png";
+import menHat2 from "../assets/images/categories/menhat2.png";
+import menCosmetic1 from "../assets/images/categories/mencosmetic1.png";
+import menCosmetic2 from "../assets/images/categories/mencosmetic2.png";
+
+
 export const products = [
   {
-    id: "0",
+    id: 0,
     image: fixed0,
     title: "Graphic Design",
     department: "English Department",
@@ -18,7 +37,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "1",
+    id: 1,
     image: fixed1,
     title: "Graphic Design",
     department: "English Department",
@@ -27,7 +46,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "2",
+    id: 2,
     image: fixed2,
     title: "Graphic Design",
     department: "English Department",
@@ -36,7 +55,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "3",
+    id: 3,
     image: fixed3,
     title: "Graphic Design",
     department: "English Department",
@@ -45,7 +64,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "4",
+    id: 4,
     image: fixed4,
     title: "Graphic Design",
     department: "English Department",
@@ -54,7 +73,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "5",
+    id: 5,
     image: fixed5,
     title: "Graphic Design",
     department: "English Department",
@@ -63,7 +82,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "6",
+    id: 6,
     image: fixed6,
     title: "Graphic Design",
     department: "English Department",
@@ -72,7 +91,7 @@ export const products = [
     colors: ["blue", "green", "orange", "black"],
   },
   {
-    id: "7",
+    id: 7,
     image: fixed7,
     title: "Graphic Design",
     department: "English Department",
@@ -80,4 +99,174 @@ export const products = [
     oldPrice: "$16.48",
     colors: ["blue", "green", "orange", "black"],
   },
+
+
+  {
+    id: 8,
+    title: "Bag Product 1",
+    category: "bags",
+    image: womenBag1,
+    gender:"women",
+    price: "$299",
+    stock: 5,
+    
+  },
+  {
+    id: 9,
+    title: "Bag Product 2",
+    category: "bags",
+    image: womenBag2,
+    gender:"women",
+    price: "$349",
+    stock: 3,
+    
+  },
+  {
+    id: 10,
+    title: "Belt Product 1",
+    category: "belts",
+    image: womenBelt1,
+    gender:"women",
+    price: "$199",
+    stock: 8,
+   
+  },
+
+   {
+    id: 11,
+    title: "Belt Product 2",
+    category: "belts",
+    image: womenBelt2,
+    gender:"women",
+    price: "$299",
+    stock: 5,
+   
+  },
+  {
+    id: 12,
+    title: "Cosmetic Product 1",
+    category: "cosmetics",
+    image: womenCosmetic1,
+    gender:"women",
+    price: "$349",
+    stock: 3,
+    
+  },
+  {
+    id: 13,
+    title: "Cosmetic Product 2",
+    category: "cosmetics",
+    image: womenCosmetic2,
+    gender:"women",
+    price: "$199",
+    stock: 8,
+  
+  },
+
+    {
+    id: 14,
+    title: "Hat Product 1",
+    category: "hats",
+    image: womenHat1,
+    gender:"women",
+    price: "$299",
+    stock: 5,
+   
+  },
+  {
+    id: 15,
+    title: "Hat Product 2",
+    category: "hats",
+    image: womenHat2,
+    gender:"women",
+    price: "$349",
+    stock: 3,
+  
+  },
+  {
+    id: 16,
+    title: "Bag Product 1",
+    category: "bags",
+    image: menBag1,
+    gender:"men",
+    price: "$199",
+    stock: 8,
+   
+  },
+
+   {
+    id: 17,
+    title: "Bag Product 2",
+    category: "bags",
+    image: menBag2,
+    gender:"men",
+    price: "$299",
+    stock: 5,
+   
+  },
+  {
+    id: 18,
+    title: "Belt Product 1",
+    category: "belts",
+    image: menBelt1,
+    gender:"men",
+    price: "$349",
+    stock: 3,
+ 
+  },
+  {
+    id: 19,
+    title: "Belt Product 2",
+    category: "belts",
+    image: menBelt2,
+    gender:"men",
+    price: "$199",
+    stock: 8,
+  
+  },
+
+   {
+    id: 20,
+    title: "Hat Product 1",
+    category: "hats",
+    image: menHat1,
+    gender:"men",
+    price: "$199",
+    stock: 8,
+  
+  },
+
+   {
+    id: 21,
+    title: "Hat Product 2",
+    category: "hats",
+    image: menHat2,
+    gender:"men",
+    price: "$299",
+    stock: 5,
+  
+  },
+
+    {
+    id: 22,
+    title: "Cosmetic Product 1",
+    category: "cosmetics",
+    image: menCosmetic1,
+    gender:"men",
+    price: "$199",
+    stock: 8,
+    
+  },
+
+   {
+    id: 23,
+    title: "Cosmetic Product 2",
+    category: "cosmetics",
+    image: menCosmetic2,
+    gender:"men",
+    price: "$299",
+    stock: 5,
+ 
+  },
+
 ];
