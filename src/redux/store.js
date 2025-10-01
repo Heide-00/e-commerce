8835ff954384476f5e495c6763d8154528dbf91b
Heide-00 <gulaslan93@gmail.com>
2025-10-02 00:04:12 +0300

    Update store.js
    store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import clientReducer from './clientSlice';
 import cartReducer from './cartSlice'; 
+import categoryReducer from '../store/reducers/categoryReducer';
 
 export const store = configureStore({
   reducer: {
     client: clientReducer,
     cart: cartReducer,
+    categories: categoryReducer,
   },
 });
